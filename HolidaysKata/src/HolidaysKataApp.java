b0c48b6d54f5ca80bf92f835fe530be5a76b12b9
samir_sayegh <samirsayegh2@gmail.com>
2016-01-10 19:45:05 +0100

    Imports and configuration

---
+++ HolidaysKata/src/HolidaysKataApp.java
@@ -0,0 +1,5 @@
+/**
+ * Created by samir on 1/10/2016.
+ */
+public class HolidaysKataApp {
+}
---
+++ HolidaysKata/test/HolidaysKataTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+/**
+ * Created by samir on 1/10/2016.
+ */
+public class HolidaysKataTest {
+
+
+}
