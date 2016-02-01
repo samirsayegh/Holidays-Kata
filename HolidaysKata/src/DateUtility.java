aaa5d4ddda63482b3f9fc52f8f5ee9876439d47d
samir_sayegh <samirsayegh2@gmail.com>
2016-02-01 19:37:14 +0100

    Implementation of the test

--- HolidaysKata/src/DateUtility.java
+++ HolidaysKata/src/DateUtility.java
@@ -1,5 +1,14 @@
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 /**
  * Created by samir on 2/1/2016.
  */
 public class DateUtility {
+
+    public boolean isValidFormat(String dateString) {
+        return true;
+    }
+
 }
